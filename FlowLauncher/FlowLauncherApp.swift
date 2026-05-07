87390f2cb1203dea1f23e2bfb6a4ce3fa2343d5d
tokorom <tokorom@gmail.com>
2026-05-07 19:04:37 +0900

    First commit

---
+++ FlowLauncher/FlowLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowLauncherApp.swift
+//  FlowLauncher
+//
+//  Created by Yuta Tokoro on 2026/05/07.
+//
+
+import SwiftUI
+
+@main
+struct FlowLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
